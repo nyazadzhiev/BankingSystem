c6212efb71ca92760f42a96b831c2ab35cc1e296
nyazadzhiev <nyazadzhiev@gmail.com>
2026-03-21 23:37:21 +0200

    add tests

---
+++ BankingSystem/BankingSystem/Tests/BankAccountTests.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankingSystem.BO;
+using BankingSystem.Exceptions;
+
+namespace BankingSystem.Tests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        private CheckingAccount _checking;
+        private SavingsAccount _savings;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _checking = new CheckingAccount("John", 1, 1000, 500);
+            _savings = new SavingsAccount("Jane", 2, 1000, 0.12m, 5000);
+        }
+
+        [TestMethod]
+        public void Deposit_ShouldIncreaseBalance()
+        {
+            _checking.Deposit(500);
+
+            Assert.AreEqual(1500, _checking.GetBalance());
+        }
+
+        [TestMethod]
+        public void Deposit_ShouldThrow_WhenNegative()
+        {
+            Assert.ThrowsException<InvalidAmountException>(() =>
+            {
+                _checking.Deposit(-10);
+            });
+        }
+
+        [TestMethod]
+        public void Savings_Deposit_ShouldThrow_WhenExceedMaxBalance()
+        {
+            Assert.ThrowsException<MaxBalanceExceededException>(() =>
+            {
+                _savings.Deposit(10000);
+            });
+        }
+
+        [TestMethod]
+        public void Withdraw_ShouldDecreaseBalance()
+        {
+            _checking.Withdraw(200);
+
+            Assert.AreEqual(800, _checking.GetBalance());
+        }
+
+        [TestMethod]
+        public void Withdraw_ShouldThrow_WhenInsufficientFunds_Savings()
+        {
+            Assert.ThrowsException<InsufficientFundsException>(() =>
+            {
+                _savings.Withdraw(2000);
+            });
+        }
+
+        [TestMethod]
+        public void Withdraw_ShouldThrow_WhenInvalidAmount()
+        {
+            Assert.ThrowsException<InvalidAmountException>(() =>
+            {
+                _checking.Withdraw(-50);
+            });
+        }
+
+        [TestMethod]
+        public void Checking_ShouldAllowOverdraftWithinLimit()
+        {
+            _checking.Withdraw(1300); // 1000 - 1300 = -300 (allowed)
+
+            Assert.IsTrue(_checking.GetBalance() < 0);
+        }
+
+        [TestMethod]
+        public void Checking_ShouldThrow_WhenOverdraftExceeded()
+        {
+            Assert.ThrowsException<OverdraftLimitExceededException>(() =>
+            {
+                _checking.Withdraw(2000);
+            });
+        }
+
+
+        [TestMethod]
+        public void Transfer_ShouldMoveMoneyBetweenAccounts()
+        {
+            var target = new CheckingAccount("Bob", 3, 500, 200);
+
+            _checking.TransferTo(target, 300);
+
+            Assert.AreEqual(700, _checking.GetBalance());
+            Assert.AreEqual(800, target.GetBalance());
+        }
+
+        [TestMethod]
+        public void Transfer_ShouldThrow_WhenInvalidAmount()
+        {
+            var target = new CheckingAccount("Bob", 3, 500, 200);
+
+            Assert.ThrowsException<InvalidAmountException>(() =>
+            {
+                _checking.TransferTo(target, -100);
+            });
+        }
+
+
+        [TestMethod]
+        public void ApplyInterest_ShouldIncreaseBalance_Monthly()
+        {
+            _savings.ApplyInterest(CompoundingMode.Monthly);
+
+            Assert.IsTrue(_savings.GetBalance() > 1000);
+        }
+
+        [TestMethod]
+        public void ApplyInterest_ShouldIncreaseBalance_Yearly()
+        {
+            _savings.ApplyInterest(CompoundingMode.Yearly);
+
+            Assert.IsTrue(_savings.GetBalance() > 1000);
+        }
+
+        [TestMethod]
+        public void ApplyInterest_ShouldThrow_WhenMaxBalanceExceeded()
+        {
+            var rich = new SavingsAccount("Rich", 4, 4900, 0.5m, 5000);
+
+            Assert.ThrowsException<MaxBalanceExceededException>(() =>
+            {
+                rich.ApplyInterest(CompoundingMode.Yearly);
+            });
+        }
+
+
+        [TestMethod]
+        public void Deposit_ShouldCreateTransaction()
+        {
+            _checking.Deposit(100);
+
+            Assert.IsTrue(_checking.Transactions.Any(t => t.Type == TransactionType.Deposit));
+        }
+
+        [TestMethod]
+        public void Withdraw_ShouldCreateTransaction()
+        {
+            _checking.Withdraw(100);
+
+            Assert.IsTrue(_checking.Transactions.Any(t => t.Type == TransactionType.Withdraw));
+        }
+    }
+}
---
+++ BankingSystem/BankingSystem/Tests/BankTests.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankingSystem.BO;
+using BankingSystem.Exceptions;
+using System;
+using System.Linq;
+
+namespace BankingSystem.Tests
+{
+    [TestClass]
+    public class BankTests
+    {
+        private Bank _bank;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _bank = new Bank();
+        }
+
+        [TestMethod]
+        public void OpenAccount_ShouldCreateSavingsAccount()
+        {
+            var account = _bank.OpenAccount("John Doe", "savings", 1000, 0.05m, 500m);
+
+            Assert.IsNotNull(account);
+            Assert.AreEqual(1000, account.GetBalance());
+            Assert.AreEqual(1, account.AccountNumber);
+        }
+
+        [TestMethod]
+        public void OpenAccount_ShouldCreateCustomerIfNotExists()
+        {
+            _bank.OpenAccount("John Doe", "checking", 500, 200);
+
+            var customers = _bank.GetAllCustomers();
+
+            Assert.AreEqual(1, customers.Count());
+            Assert.AreEqual("John Doe", customers.First().Name);
+        }
+
+        [TestMethod]
+        public void OpenAccount_ShouldThrow_WhenInitialBalanceNegative()
+        {
+            Assert.ThrowsException<InvalidAmountException>(() =>
+            {
+                _bank.OpenAccount("John Doe", "savings", -100, 0.05m, 500m);
+            });
+        }
+
+        [TestMethod]
+        public void FindAccount_ShouldReturnCorrectAccount()
+        {
+            var acc = _bank.OpenAccount("John Doe", "checking", 500, 200);
+
+            var found = _bank.FindAccount(acc.AccountNumber);
+
+            Assert.AreEqual(acc.AccountNumber, found.AccountNumber);
+        }
+
+        [TestMethod]
+        public void FindAccount_ShouldThrow_WhenNotFound()
+        {
+            Assert.ThrowsException<AccountNotFoundException>(() =>
+            {
+                _bank.FindAccount(999);
+            });
+        }
+
+        [TestMethod]
+        public void CloseAccount_ShouldRemoveAccount_WhenBalanceZero()
+        {
+            var acc = _bank.OpenAccount("John Doe", "checking", 0, 200);
+
+            _bank.CloseAccount(acc.AccountNumber);
+
+            var accounts = _bank.ListAccounts();
+            Assert.AreEqual(0, accounts.Count);
+        }
+
+        [TestMethod]
+        public void CloseAccount_ShouldThrow_WhenBalanceNotZero()
+        {
+            var acc = _bank.OpenAccount("John Doe", "checking", 100, 200);
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                _bank.CloseAccount(acc.AccountNumber);
+            });
+        }
+
+        [TestMethod]
+        public void ListAccounts_ShouldReturnAllAccounts()
+        {
+            _bank.OpenAccount("John Doe", "checking", 100, 200);
+            _bank.OpenAccount("Jane Doe", "savings", 200, 0.05m, 100);
+
+            var accounts = _bank.ListAccounts();
+
+            Assert.AreEqual(2, accounts.Count);
+        }
+
+        [TestMethod]
+        public void GenerateStatement_ShouldThrow_WhenInvalidDateRange()
+        {
+            var acc = _bank.OpenAccount("John Doe", "checking", 100, 200);
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                _bank.GenerateStatement(acc.AccountNumber, DateTime.Now, DateTime.Now.AddDays(-1));
+            });
+        }
+
+        [TestMethod]
+        public void FindCustomer_ShouldReturnCorrectCustomer()
+        {
+            _bank.OpenAccount("John Doe", "checking", 100, 200);
+            var customer = _bank.GetAllCustomers().First();
+
+            var found = _bank.FindCustomer(customer.Id);
+
+            Assert.AreEqual(customer.Id, found.Id);
+        }
+
+        [TestMethod]
+        public void FindCustomer_ShouldThrow_WhenNotFound()
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                _bank.FindCustomer(999);
+            });
+        }
+    }
+}
